26596a75971959886b62461571b0dfe1866c41ec
Adrian <adria@example.com>
2026-06-11 21:58:17 +0800

    blah

--- src/search.cpp
+++ src/search.cpp
@@ -53,6 +53,9 @@ namespace Stockfish {
 
 static constexpr std::array<int, 16> lmrDivisor = {3307, 2930, 2874, 2818, 3215, 3225, 3224, 2782,
                                                    2858, 2919, 3088, 3275, 3180, 2868, 3006, 3599};
+static constexpr int NUM  = 158;
+static constexpr int MINC = 5;
+static constexpr int MAXC = 12;
 
 namespace TB = Tablebases;
 
@@ -1147,7 +1150,7 @@ moves_loop:  // When in check, search starts here
                 int   captHist = captureHistory[movedPiece][move.to_sq()][type_of(capturedPiece)];
 
                 // Futility pruning for captures
-                if (!givesCheck && lmrDepth < 7)
+                if (!givesCheck && lmrDepth < std::clamp(NUM / depth, MINC, MAXC))
                 {
                     Value futilityValue = ss->staticEval + 231 + 232 * lmrDepth
                                         + PieceValue[capturedPiece] + 131 * captHist / 1024;
