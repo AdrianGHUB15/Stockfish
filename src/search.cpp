7bf17bc4475c20c5afce6922a0e20ac11dac6a72
Adrian <adria@example.com>
2026-06-24 17:36:56 +0800

    less nodes

--- src/search.cpp
+++ src/search.cpp
@@ -845,7 +845,8 @@ Value Search::Worker::search(
         depth++;
     if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 173)
         depth--;
-
+    if (ss->staticEval > (ss - 6)->staticEval + 75 && opponentWorsening && depth >= 15)
+        depth --;
     // At non-PV nodes we check for an early TT cutoff
     if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
