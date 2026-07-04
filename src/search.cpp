5dbd45323f7e060de61ace56ac8c48742c46c748
Adrian <adria@example.com>
2026-07-04 09:26:44 +0800

    blah

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our threshold
-            if (moveCount >= (3 + depth * depth) / (2 - improving))
+            if (moveCount >= (3 + depth * depth) / (2 + improving))
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search
