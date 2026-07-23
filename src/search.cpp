ed3303668bf7c7365fa562678a877ea211c273f6
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-07-23 08:00:18 +0800

    after stc passed

--- src/search.cpp
+++ src/search.cpp
@@ -1502,8 +1502,8 @@ moves_loop:  // When in check, search starts here
                 }
 
                 // Reduce other moves if we have found at least one score improvement
-                if (depth > 3 && depth < 12 && !is_decisive(value))
-                    depth -= 3;
+                if (depth > 2 && depth < 13 && !is_decisive(value))
+                    depth -= 2;
 
                 assert(depth > 0);
                 alpha = value;  // Update alpha! Always alpha < beta
