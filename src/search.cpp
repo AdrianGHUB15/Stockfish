69dd02a00a1a2c67083932532c53e00d271fbf5c
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-07-27 18:52:26 +0800

    spec LTC

--- src/search.cpp
+++ src/search.cpp
@@ -846,7 +846,7 @@ Value Search::Worker::search(
     // false otherwise. The improving flag is used in various pruning heuristics.
     // Similarly, opponentWorsening is true if our static evaluation is better
     // for us than at the last ply.
-    improving         = ss->staticEval > (ss - 2)->staticEval;
+    improving         = ss->staticEval > (ss - 2)->staticEval + 25;
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
     // Hindsight adjustment of reductions based on static evaluation difference.
