1097526c5d4fcb4f65ed5095023ff4a8cb1895d8
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-08-01 09:51:50 +0800

    matetrack improvement, eval only

--- src/search.cpp
+++ src/search.cpp
@@ -979,7 +979,7 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 19 && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta)
-        && !is_win(eval))
+        && eval <= 2000)
     {
         Value futilityMult = std::min(45 + depth * 4, 85);
         futilityMult -= 20 * !ss->ttHit;
