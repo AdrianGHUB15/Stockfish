e451db480d8095322787f38e41cfac689a133ac9
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-08-05 19:54:43 +0800

    take 3

--- src/search.cpp
+++ src/search.cpp
@@ -973,8 +973,8 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 483 - 318 * depth * depth)
-        return qsearch<NonPV>(pos, ss, alpha, beta);
+    if (!is_win(eval) && eval < alpha - 483 - 318 * depth * depth)
+        return qsearch<nodeType>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
     // The depth condition is important for mate finding.
