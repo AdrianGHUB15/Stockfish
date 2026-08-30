ec5b2b892c0ec130337c375e4d63bd243c2beb25
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-08-30 22:53:38 +0800

    now rebased

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 13. A small ProbCut idea
-    probCutBeta = beta + 428;
+    probCutBeta = beta + 428 - improving * 123;
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
