260708a68585e8423688144c126f14085c9bb054
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-01 17:29:47 +0800

    try pv node aswell, since we have seek mate to prevent mate scores to be returned

--- src/search.cpp
+++ src/search.cpp
@@ -988,7 +988,7 @@ Value Search::Worker::search(
 
     // Step 8. Razoring
     // If eval is really low, skip search entirely and return the qsearch value
-    if (eval < alpha - 482 * depth * (depth * PvNode)) {
+    if (eval < alpha - 482 * depth * depth) {
         constexpr NodeType childNodeType = nodeType == NonPV ? NonPV : PV;
         return qsearch<childNodeType>(pos, ss, alpha, beta);
     }
