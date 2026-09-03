4ed2496aed201599c8ee31df6e6567687d72b0cd
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-03 16:55:59 +0800

    1 less mate but 30 more bestmates

--- src/search.cpp
+++ src/search.cpp
@@ -866,7 +866,7 @@ Value Search::Worker::search(
     // Hindsight adjustment of reductions based on static evaluation difference
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 166)
+    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 166 && !seekMate)
         depth--;
 
     // Step 6. At non-PV nodes we check for an early TT cutoff. Note that we
