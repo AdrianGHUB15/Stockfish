5ede8daa795b585d878a51a26f9336c745c630ce
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-04 20:44:57 +0800

    only razor at depth 1, take 1

--- src/search.cpp
+++ src/search.cpp
@@ -988,7 +988,7 @@ Value Search::Worker::search(
 
     // Step 8. Razoring
     // If eval is really low, skip search entirely and return the qsearch value
-    if (!PvNode && eval < alpha - 482 * depth * depth)
+    if (eval < alpha - 482 && std::abs(eval) < 1000 && depth == 1)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 9. Futility pruning: child node
