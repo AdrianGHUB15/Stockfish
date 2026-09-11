9c72a2bf947f7bf0ff8f8ef5aab2a777da2ceec2
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-11 09:19:23 +0800

    anyone can stop if elo is more than 10

--- src/search.cpp
+++ src/search.cpp
@@ -864,12 +864,6 @@ Value Search::Worker::search(
     improving         = ss->staticEval > (ss - 2)->staticEval;
     opponentWorsening = ss->staticEval > -(ss - 1)->staticEval;
 
-    // Hindsight adjustment of reductions based on static evaluation difference
-    if (priorReduction >= 3 && !opponentWorsening)
-        depth++;
-    if (priorReduction >= 2 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 166)
-        depth--;
-
     // Step 6. At non-PV nodes we check for an early TT cutoff. Note that we
     //         always check the validity of the TT value because of access races.
     if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
