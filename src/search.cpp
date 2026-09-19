2465458262d62012419d0741de1814cad6b36a26
Adrian Ladoni <274005880+AdrianGHUB15@users.noreply.github.com>
2026-09-19 10:46:46 +0800

    now do razoring on pv nodes too

--- src/search.cpp
+++ src/search.cpp
@@ -1005,9 +1005,10 @@ Value Search::Worker::search(
 
     // Step 8. Razoring
     // If eval is really low, skip search entirely and return the qsearch value
-    if (!cutNode && eval < alpha - 342 * depth && !seekMate && std::abs(eval) < 2000 && std::abs(beta) < 2000)
+    if (!cutNode && eval < alpha - 342 * depth && !seekMate && std::abs(eval) < 2000 && std::abs(beta) < 2000) {
         constexpr NodeType childNodeType = nodeType == NonPV ? NonPV : PV;
         return qsearch<childNodeType>(pos, ss, alpha, beta);
+    }
 
     // Step 9. Futility pruning: child node
     // The depth condition is important for mate finding. It should NOT be tuned.
